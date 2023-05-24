8cfa86296ee78891a8573a4d1df410e801b79661
chamito444 <134552889+chamito444@users.noreply.github.com>
2023-05-24 16:43:15 -0400

    datos pedidos

---
+++ CLASE 12/datos.py
@@ -0,0 +1,9 @@
+#EJEMPLO DE USP GITHUB
+print("INGRESO DE DATOS")
+print("-----------------")
+nombre = input("Ingrese su nombre -> ")
+edad = input("Ingrese su edad -> ")
+print("----------------------------")
+print(f"Su nombre es -> {nombre}")
+print(f"Su edad es -> {edad}")
+print("PROGRAMA FINALIZADO 🤐") 
